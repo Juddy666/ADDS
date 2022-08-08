7208c1c582421c471e1aa744c266e947dcaa0777
Juddy666 <a1833565@adelaide.edu.au>
2022-08-08 14:01:26 +0930

    removed files

--- main-2-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-extern int max_element(int array[], int n);
-int main(int argc,char **argv)
-{
-int array[6] = {3,5,4,5,-1,9};
-std::cout << "The maximum is: " << max_element(array,6) << std::endl;
-return 0 ;
-}
--- main-2-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-extern bool is_ascending(int array[], int n);
-
-int main(int argc,char **argv){
-int array[6] = {0,2,4,5,7,9};
- std::cout << "The array ascends: " << std::boolalpha << is_ascending(array,6)<< std::endl;
-return 0 ;
-}
