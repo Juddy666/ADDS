838c55a13a8393698d7c27856bcfbb3546f67cf3
Juddy666 <a1833565@adelaide.edu.au>
2022-08-24 16:47:15 +0930

    q1p2

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 
 int *left_shift(int *vals, int len, int amount){
-
+    int *new_ptr = new int[len];
+    new_ptr = vals;
     for(int i =0; i<(len-amount);i++){
-        *(vals+i) = *(vals+i+amount);
+        *(new_ptr+i) = *(vals+i+amount);
     }
 
     for(int i =len-amount; i<len;i++){
-        *(vals+i) =0;
+        *(new_ptr+i) =0;
     }
 
-    return vals;
+    return new_ptr;
 }
