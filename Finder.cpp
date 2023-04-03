b89c022dadab363a7af6c6b96603424265eb3bf3
Juddy666 <a1833565@adelaide.edu.au>
2023-04-03 11:26:48 +0930

    upload 2

--- Finder.cpp
+++ Finder.cpp
@@ -5,15 +5,15 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result(s2.size(),-1);
-        int from = 0;
+        size_t from = 0;
         for(size_t i = 1; i <= s2.size(); i++) {
             string index = s2.substr(0,i);
             size_t found = s1.find(index, from);
             if (found != string::npos) {
-                result.push_back(found);
-                from = index;
+                result[i-1] = found;
+                from = found;
             } else {
-                return result;
+                break;
             }
         }
         return result;
