838f1624d40d495a206820372ef5b16637296fc7
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-04-06 14:56:50 +0930

    Add files via upload

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,5 +3,13 @@
 #include <vector>
 
 std::vector<int> BubbleSort::sort(std::vector<int> list){
-    
+    for (int i = vector.size() -1; i>1;i--){
+        for(int j = 0;j<i;j++){
+            if(vector.at(j)>vector.at(j+1)){
+                int temp = vector.at(j);
+                vector.at(j) = vector.at(j+1);
+                vector.at(j+1) = temp;
+            }
+        }
+    }
 }
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "BubbleSort.h"
+#include <vector>
+
+using namespace std;
+int main(void){
+    BubbleSort mySort;
+    
+    std::vector<int>  myVector{7,4,2,10,3,1};
+    
+    
+    return 0;
+};
