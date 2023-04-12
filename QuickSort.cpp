59ff560b41491208ff59e558b34cfb74a7b67442
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-04-12 14:34:12 +0930

    Add files via upload

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,6 +1,7 @@
 #include "QuickSort.h"
 #include <vector>
-
+#include <iostream>
+using namespace std;
 std::vector<int>  QuickSort::sort(std::vector<int> list){
     
     if (list.empty()) {
@@ -26,22 +27,34 @@ std::vector<int>  QuickSort::sort(std::vector<int> list){
     
     int pivot = list[2];
     
+
     std::vector<int> lesser;
     std::vector<int> greater;
     
     
     for(int i =0;i<list.size();i++){
-        if (list[i] < pivot){
+        if (list[i] < pivot&&i!=2){
             lesser.push_back(list[i]);
         }
-        else{
+        else if((list[i]>=pivot)&&i!=2){
             greater.push_back(list[i]);
         }
     }
+    // cout << "Lesser: " <<endl;
+    // for(int i=0; i < lesser.size();i++){
+    //     std::cout << lesser[i] << " ";
+    // }
+    // std::cout<<endl;
 
+
+    // cout << "Greater: " <<endl;
+    // for(int i=0; i < greater.size();i++){
+    //     std::cout << greater[i] << " ";
+    // }
+    std::cout<<endl;
     lesser = sort(lesser);
     greater = sort(greater);
-
+    lesser.push_back(list[2]);
     lesser.insert(lesser.end(),greater.begin(),greater.end());
 
     return lesser;
