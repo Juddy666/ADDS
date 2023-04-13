ed69d8f250a66626591b02ff7239e5a7c95ea3dd
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-04-13 14:49:53 +0930

    Add files via upload

---
+++ RecursiveBinarySearch.h
@@ -0,0 +1,11 @@
+#ifndef RECURSIVEBINARYSEARCH
+#define RECURSIVEBINARYSEARCH
+
+#include <vector>
+
+class RecursiveBinarySearch{
+    public:
+        bool search(std::vector<int> list, int target);
+
+}
+#endif //RECURSIVEBINARYSEARCH
