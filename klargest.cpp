a39ca6a33fe3abb94e3be804fc4021670198e616
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-05-18 16:48:02 +0930

    Add files via upload

--- klargest.cpp
+++ klargest.cpp
@@ -1,18 +1,24 @@
 #include <queue>
 #include <vector>
-
+#include <iostream>
 int kth_largest(std::vector<int> values, int k){
-    int length = sizeof(values);
-    if(k>length){
-        k = length;
-    }
+    int length = values.size();
     std::priority_queue<int> myqueue;
     for(int i =0;i<length;i++){
         myqueue.push(values[i]); 
     }
 
-    for(int j =0;j<=k-1;j++){
+    for(int j =0;j<k-1;j++){
         myqueue.pop();
     }
     return myqueue.top();
 };
+
+// int main(){
+//     std::vector<int> myvector{ 10, 20, 30 };
+
+//     int ans;
+//     ans = kth_largest(myvector,1);
+//     std::cout << ans << std::endl;
+//     return 0;
+// }
